444d0928e560d56a6515cad2a3c5b9b5f2097308
Timmy <tim_watts@ymail.com>
2024-04-30 22:27:58 -0600

    updates

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -46,10 +46,10 @@ export default function Home() {
         <Grid container sx={{ py: 8 }}>
           <Grid xs={8} sx={{ display: "flex", flexDirection: "column", gap: 4, margin: "0 auto" }}>
             <h1 style={{ color: theme.palette.primary.main }}>
-              Embrace Positivity and Embody Your Best Self
+              {'Embrace Positivity and Embody Your Best Self'}
             </h1>
             <p>
-              <p>It's time to ditch the negativity holding you back.</p>
+              <p>{'It\'s time to ditch the negativity holding you back.'}</p>
               <p>As a Positivity and Embodiment Coach, I help guys like you rediscover their confidence and build a life they're excited about.
                 We'll tackle the mental roadblocks keeping you stuck and equip you with practical tools to manage anxiety, boost your self-esteem, and achieve your goals.</p>
 
@@ -60,17 +60,17 @@ export default function Home() {
                 {'Get Started'}
               </Button>
               <Dialog open={open} onClose={handleClose} sx={{ textAlign: "center" }}>
-                <DialogTitle >My site is almost ready.</DialogTitle>
+                <DialogTitle>{'My site is almost ready.'}</DialogTitle>
                 <Divider />
                 <DialogContent sx={{ textAlign: "center", px: 8 }}>
                   <>
                     <p>
-                      <strong>Please call or email me for now:</strong>
+                      {'Please call or email me for now'}
                     </p>
                     <p>
-                      Phone: {'801-739-4598'}
+                      {'Phone: 801-739-4598'}
                       <br />
-                      Email: {'tim_watts@ymail.com'}
+                      {'Email: tim_watts@ymail.com'}
                     </p>
                     <p>Contact me to get a <strong style={{ color: theme.palette.primary.main }}>free</strong> 30 min consultation</p>
                   </>
