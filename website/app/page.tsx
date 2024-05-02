192206d8b17d6b1b4514b98e6294f1d7bfe99871
Timmy <tim_watts@ymail.com>
2024-05-01 18:45:49 -0600

    added contact form

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -33,53 +33,56 @@ export default function Home() {
   };
 
   return (
-    <Container maxWidth={false} className="container">
-      <Box maxWidth="lg"
-        sx={{
-          display: "flex",
-          flexDirection: {
-            sm: "column",
-            lg: "row"
-          },
-          margin: "0 auto",
-        }}>
-        <Grid container sx={{ py: 8 }}>
-          <Grid xs={8} sx={{ display: "flex", flexDirection: "column", gap: 4, margin: "0 auto" }}>
-            <h1 style={{ color: theme.palette.primary.main }}>
-              {'Embrace Positivity and Embody Your Best Self'}
-            </h1>
-            <div>
-              <p>{'It\'s time to ditch the negativity holding you back.'}</p>
-              <p>{'As a Positivity and Embodiment Coach, I help guys like you rediscover their confidence and build a life they\'re excited about. We\'ll tackle the mental roadblocks keeping you stuck and equip you with practical tools to manage anxiety, boost your self-esteem, and achieve your goals.'}</p>
-              <p>{'Whether you\'re looking to improve your career, relationships, or overall well-being, this is your chance to take charge and become the best version of yourself.'}</p>
-            </div>
-            <p style={{ marginTop: "24px" }}>
-              <Button variant="contained" size="large" color="primary" onClick={handleClickOpen}>
-                {'Get Started'}
-              </Button>
-              <Dialog open={open} onClose={handleClose} sx={{ textAlign: "center" }}>
-                <DialogTitle>{'My site is almost ready.'}</DialogTitle>
-                <Divider />
-                <DialogContent sx={{ textAlign: "center", px: 8 }}>
-
-                    <p>
-                      {'Please call or email me for now'}
-                    </p>
-                    <p>
-                      {'Phone: 801-739-4598'}
-                      <br />
-                      {'Email: tim_watts@ymail.com'}
-                    </p>
-                    <p>{'Contact me to get a '}<strong style={{ color: theme.palette.primary.main }}>{'free'}</strong>{' 30 min consultation'}</p>
-                </DialogContent>
-                <DialogActions sx={{ display: "flex", justifyContent: "center" }}>
-                  <Button onClick={handleClose}>Close</Button>
-                </DialogActions>
-              </Dialog>
-            </p>
-          </Grid>
+    <Container maxWidth={false} className="container" sx={{ pt: 8 }}>
+    <Grid container maxWidth="lg"  sx={{ margin: { md: '-8px', lg: 'auto', }, pb: 8 }}>
+        <Grid item xs={12} md={7} sx={{ pr: { sm: 0, md: 8 } }}>
+          <Box sx={{ height: "150px", width: "150px", marginBottom: 2, borderRadius: "50%", border: "2px solid white", backgroundColor: theme.palette.secondary.main, overflow: "hidden", position: "relative" }}>
+            <Image src="/Coach_Tim.png" alt="Logo" width={150} height={230} style={{ position: "relative", left: "-10px", top: "10px", }} />
+          </Box>
+          <h1 style={{ color: theme.palette.primary.main }}>
+            {'Embrace Positivity and Embody Your Best Self'}
+          </h1>
+          <div>
+            <p>{'It\'s time to ditch the negativity holding you back.'}</p>
+            <p>{'As a Positivity and Embodiment Coach, I help guys like you rediscover their confidence and build a life they\'re excited about. We\'ll tackle the mental roadblocks keeping you stuck and equip you with practical tools to manage anxiety, boost your self-esteem, and achieve your goals.'}</p>
+            <p>{'Whether you\'re looking to improve your career, relationships, or overall well-being, this is your chance to take charge and become the best version of yourself.'}</p>
+          </div>
+          {/* <p style={{ marginTop: "24px" }}>
+            <Button variant="contained" size="large" color="primary" onClick={handleClickOpen}>
+              {'Get Started'}
+            </Button>
+            <Dialog open={open} onClose={handleClose} sx={{ textAlign: "center" }}>
+              <DialogTitle>{'My site is almost ready.'}</DialogTitle>
+              <Divider />
+              <DialogContent sx={{ textAlign: "center", px: 8 }}>
+                <ContactForm />
+                <p>
+                  {'Please call or email me for now'}
+                </p>
+                <p>
+                  {'Phone: 801-739-4598'}
+                  <br />
+                  {'Email: tim_watts@ymail.com'}
+                </p>
+                <p>{'Contact me for a '}<strong style={{ color: theme.palette.primary.main }}>{'free'}</strong>{' 30 min consultation'}</p>
+              </DialogContent>
+              <DialogActions sx={{ display: "flex", justifyContent: "center" }}>
+                <Button onClick={handleClose}>Close</Button>
+              </DialogActions>
+            </Dialog>
+          </p> */}
         </Grid>
-      </Box>
+        <Grid item xs={12} md={5} sx={{ display: "flex", justifyContent: "flex-end", }}>
+          <Card sx={{ width: "100%", height: "100%", maxWidth: { sm: "100%", md: 425}, }} elevation={12}>
+            <CardContent sx={{ mt: 2, textAlign: "center", }}>
+              {'Contact me for a '}<strong style={{ color: theme.palette.primary.main }}>{'free'}</strong>{' 30 min consultation'}
+            </CardContent>
+            <CardContent sx={{ px: 6, mb: 4 }}>
+              <ContactForm />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Container >
   );
 }
--- website/components/ContactForm.tsx
+++ website/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 import { useForm, ValidationError } from "@formspree/react";
-import { Button, TextField, FormControl, FormHelperText } from '@mui/material';
+import { Button, TextField, FormControl, FormHelperText, Box } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Item from '@mui/material/Grid';
 
@@ -13,33 +13,27 @@ export default function ContactForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Grid container spacing={2}>
-        <FormControl>
-          <Grid item xs={6}>
-            <Item>
-              <TextField id="name" type="name" name="name" label="Full Name" />
-            </Item>
-          </Grid>
-          <Grid xs={6}>
-            <TextField id="email" type="email" name="email" label="Email Address" />
-          </Grid>
+        <FormControl fullWidth>
+          <TextField id="name" type="name" name="name" label="Full Name" margin="normal" />
+        </FormControl>
+        <FormControl fullWidth>
+          <TextField id="email" type="email" name="email" label="Email Address" margin="normal" />
           <FormHelperText error>
             <ValidationError prefix="Email" field="email" errors={state.errors} />
           </FormHelperText>
         </FormControl>
-        <FormControl>
-          <TextField id="message" name="message" label="Message" multiline />
+        <FormControl fullWidth>
+          <TextField id="message" name="message" label="Message" multiline rows={4} margin="normal"/>
           <FormHelperText error>
             <ValidationError prefix="Message" field="message" errors={state.errors} />
           </FormHelperText>
         </FormControl>
-        <Button type="submit" disabled={state.submitting}>
+        <Button fullWidth variant="contained" size="large" type="submit" sx={{ mt: 1 }} disabled={state.submitting} >
           Submit
         </Button>
         <FormHelperText error>
           <ValidationError errors={state.errors} />
         </FormHelperText>
-      </Grid>
     </form>
   );
 }
