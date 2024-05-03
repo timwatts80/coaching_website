0831a0020be6253d97820d57902a63c0cb8d1390
Timmy <tim_watts@ymail.com>
2024-05-02 21:21:16 -0600

    update

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
   title: "Tim Watts Coaching",
   description: "No More Mr. Nice Guy Certified Coach; Positivity and Empowerment Coach",
 };
-
 const logoPath = "/timwatts_logo-WHITE.svg";
 
 export default function RootLayout({
@@ -31,15 +30,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head> */}
         <Analytics />
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
