9c17c520e92e1005648716c30bd1c1f027be6e83
Timmy <tim_watts@ymail.com>
2024-05-02 21:14:45 -0600

    favicon update

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Head>
+        {/* <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -39,7 +39,7 @@ export default function RootLayout({
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
-        </Head>
+        </Head> */}
         <Analytics />
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
